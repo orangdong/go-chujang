ea5a8d8ffc12653634bee3e00a563ee0754b55fe
orangdong <dev.naftaliaviel@gmail.com>
2024-04-16 20:41:25 +0700

    user route

--- app/routes/main.go
+++ app/routes/main.go
@@ -9,4 +9,5 @@ func SetupRoutes(app *fiber.App, db *sqlx.DB) {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{"status": "success", "message": "go-chujang ready!"})
 	})
+	UserRoutes(app, db)
 }
--- app/routes/users.route.go
+++ app/routes/users.route.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/jmoiron/sqlx"
+	"github.com/orangdong/go-chujang/app/handlers"
+)
+
+func UserRoutes(app *fiber.App, db *sqlx.DB) {
+
+	users := app.Group("/users")
+	userHandler := handlers.NewUserHandler(db)
+	users.Get("/", userHandler.GetUsers)
+}
