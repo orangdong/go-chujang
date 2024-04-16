aff76420d8ce116b966b7e13a4005d2e629d8a52
orangdong <dev.naftaliaviel@gmail.com>
2024-04-16 20:41:36 +0700

    create table users

---
+++ database/migrations/20240408014735_create_users_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR NOT NULL,
+  username VARCHAR NOT NULL,
+  password VARCHAR NOT NULL,
+  avatar TEXT
+);
